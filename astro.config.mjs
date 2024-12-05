266b5453f60d9464e8743afc912c7119935c5c5e
jynso <ecwt26@gmail.com>
2024-12-06 06:56:44 +0800

    something broke

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jyynso.github.io'
+    site: 'https://jyynso.github.io',
+    output: 'static'
 });
