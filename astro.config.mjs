6223e8e21753ebec3daa07725a5c84b967821d65
jynso <ecwt26@gmail.com>
2024-12-10 20:57:42 +0800

    overwrite domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // site: 'https://jyynso.github.io',
-    site: 'https://ecwt.github.io',
+    site: 'https://jyynso',
     output: 'static'
 });
