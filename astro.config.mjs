fde737339cd7245d67a66b96dfe67984519b5777
jynso <ecwt26@gmail.com>
2024-12-10 21:10:29 +0800

    changed domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // site: 'https://jyynso.github.io',
-    site: 'https://jyynso.github.io/ecwt.github.io',
-    output: 'static',
+    site: 'https://jyynso.github.io'
 });
